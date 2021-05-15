3aac9505c80199f40a56f189c02f2d9c8c1373d5
Gabriel Andreghetto <gabrielandreghetto@gmail.com>
2021-05-15 03:40:42 -0300

    Add routes and User model

---
+++ routes/api/users.js
@@ -0,0 +1,57 @@
+const express                     = require('express');
+const gravatar                    = require('gravatar');
+const bcrypt                      = require('bcryptjs');
+const { check, validationResult } = require('express-validator/check');
+
+const User = require('../../models/User');
+
+const router = express.Router();
+
+router.post('/', [
+    check('name', 'name is required').not().isEmpty(),
+    check('email', 'enter valid email').isEmail(),
+    check('password', 'enter a password with at least 6 characters').isLength({ min: 6 })
+], 
+async (req, res) => {
+    const errors = validationResult(req);
+
+    if(!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+    }
+
+    const { name, email, password } = req.body;
+
+    try {
+        let user = await User.findOne({ email });
+
+        if (user) {
+            return res.status(400).json({ errors: [{ msg: 'User already exists' }] });
+        }
+
+        const avatar = gravatar.url(email, {
+            s: '200',
+            r: 'pg',
+            d: 'mm'
+        });
+
+        user = new User({
+            name,
+            email,
+            avatar,
+            password
+        });
+
+        const salt = await bcrypt.genSalt(10);
+
+        user.password = await bcrypt.hash(password, salt);
+
+        await user.save();
+        
+        res.send('user registered');
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send('server error');
+    }
+});
+
+module.exports = router;
